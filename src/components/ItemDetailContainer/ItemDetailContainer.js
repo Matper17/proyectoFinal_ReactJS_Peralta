9f1b2038002c53d093bd2ff6e5e92a13884fb0f4
Matper17 <matiasperalta992@gmail.com>
2023-05-01 15:10:09 -0300

    Correcciones en ItemListContainer

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -2,9 +2,20 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { pedrirProductosPorId } from "../helpers/pedirDatos"
 import ItemDetail from "../ItemDetail/ItemDetail";
+import {collection, getDocs, query, where} from "firebase/firestore"; 
+import {db} from "../../firebase/config"; 
+// import Swal from "sweetalert2"
+
+// const warningAlert = (tittle, message) =>{
+//     Swal.fire({
+//         tittle: tittle, 
+//         text: message, 
+//         icon: "Warning"
+//     })
+// }
 
 const ItemDetailConteiner = () =>{
-    const [item, setItem] = useState(null)
+    const [item, setItem] = useState([])
     const [loading, setLoading] = useState (true)
     const {itemId} = useParams()
 
@@ -18,6 +29,26 @@ const ItemDetailConteiner = () =>{
                 setLoading(false)
             })
     }, [itemId])
+
+    const refProd = collection(db, "Productos"); 
+    const refProdquery = itemId
+    ? query(refProd, where("category", "==", itemId))
+    : refProd
+
+    getDocs (refProdquery)
+    .then ((resp) =>{
+        const docs = resp.docs.map((doc) =>{
+            return{...doc.data(), id: doc.id}
+        })
+        setItem(docs)
+    })
+    // .catch((error) =>{
+    //     warningAlert("Uups ocurrio un error!", error)
+    // })
+    .finally(() =>{
+        setLoading(false)
+    })
+
     return(
         <div>
             {
